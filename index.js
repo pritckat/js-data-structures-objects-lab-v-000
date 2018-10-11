89c49581d3025a49ee21ed73bf15f78618ca03c1
Katrina Pritchard <pritckat@gmail.com>
2018-10-11 22:53:09 +0000

    Done.

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
   const newDriver = {...driver};
-  delete newDriver.key;
+  delete newDriver[key];
   return newDriver;
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
-  return delete driver.key;
+  delete driver[key];
+
+  return driver;
 }
